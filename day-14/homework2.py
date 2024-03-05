2f8500fa7930123b41b264a2e766185297be5992
pankve1a <maybegenius10@gmail.com>
2024-03-05 23:41:21 +0400

    commit

--- day-14/homework2.py
+++ day-14/homework2.py
@@ -1,8 +1,6 @@
-my_array = []
+list = []
+for i in range(50, 100 + 1):
+    list.append(i)
+print(list)
 
-for i in range(50, 101):
-    my_array.append(i)
-
-print("Array with negative indexes:")
-for i in range(-1, -len(my_array)-1, -1):
-    print(my_array[i])
+print(list[-35: -15])
--- day-14/homework3.py
+++ day-14/homework3.py
@@ -1,8 +1,20 @@
 num1 = int(input("Enter the first integer: "))
 num2 = int(input("Enter the second integer: "))
 
-start = min(num1, num2)
-end = max(num1, num2)
+# start = min(num1, num2)
+# end = max(num1, num2)
+
+start = 0
+end = 0
+if num1 < num2:
+    end += num2
+    start += num1
+if num1 > num2:
+    end += num1
+    start += num2
+
+# print(start)
+# print(end)
 
 for i in range(start, end + 1):
     if i % 5 == 0:
