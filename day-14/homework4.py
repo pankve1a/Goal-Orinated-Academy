bedb358e24475f59441d43a52c1dfbdc35d7c073
pankve1a <maybegenius10@gmail.com>
2024-03-05 22:49:49 +0400

    commit

---
+++ day-14/homework4.py
@@ -0,0 +1,9 @@
+names_list = []
+
+age = int(input("Please enter your age: "))
+
+if age > 18:
+    name = input("Please enter your name: ")
+    names_list.append(name)
+
+print("Names list:", names_list)
