f8e9cb0073b092e27ad260609b075bd669a558ec
pankve1a <maybegenius10@gmail.com>
2024-03-05 23:49:57 +0400

    commit

--- day-14/homework4.py
+++ day-14/homework4.py
@@ -1,9 +1,10 @@
 names_list = []
 
 age = int(input("Please enter your age: "))
-
 if age > 18:
     name = input("Please enter your name: ")
     names_list.append(name)
+    print("Names list:", names_list)
+else:
+    print("your age is not valid.")
 
-print("Names list:", names_list)
